cb3df8d771dd5157b2b99de9ea5f8c12c477930b
Leigh Scheepers <86415012+bee46137@users.noreply.github.com>
2023-03-02 12:15:07 +0200

    adding google auth

--- index.js
+++ index.js
@@ -86,7 +86,8 @@ class Application {
 
         this.ui.start('#firebaseui-auth-container', {
             signInOptions: [
-              firebase.auth.EmailAuthProvider.PROVIDER_ID
+              firebase.auth.EmailAuthProvider.PROVIDER_ID,
+              firebase.auth.GoogleAuthProvider.PROVIDER_ID
             ],
             // Other config options...
           });
